b018eaee6eec8e70665679bc32d692bb3ab05ccb
JokerPro <zybancao@163.com>
2024-08-17 15:24:37 +0800

    创建公用类

---
+++ springboot/src/main/java/com/example/entity/Account.java
@@ -0,0 +1,40 @@
+package com.example.entity;
+
+public class Account {
+    private Integer id;
+    private String username;
+    private String password;
+    private Integer name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getName() {
+        return name;
+    }
+
+    public void setName(Integer name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
