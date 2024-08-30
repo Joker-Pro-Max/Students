58072ce747c486683c4ea040792036b4981cf4a1
Joker-Pro-Max <zybancao@163.com>
2024-08-30 19:06:02 +0800

    梭哈

---
+++ springboot/src/main/java/com/example/MyApplication.java
@@ -0,0 +1,21 @@
+//package com.example;
+//
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//@SpringBootApplication
+//@MapperScan("com.example.mapper")
+//public class MyApplication extends SpringBootServletInitializer {
+//
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(MyApplication.class);
+//    }
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(MyApplication.class, args);
+//    }
+//}
